6e395ce1b2a5503c1074e10de82953e45ef3eb1b
Alex <aprockhill206@gmail.com>
2020-01-22 21:00:49 -0800

    train work

--- 05_visualization.py
+++ 05_visualization.py
@@ -5,7 +5,9 @@ from swann.utils import (get_config, get_layout, get_events,
                          my_events, select_events)
 from swann.preprocessing import (apply_ica,
                                  mark_autoreject, slowfast2epochs_indices)
-from swann.viz import plot_beta_bursting, plot_power, plot_spectrogram
+from swann.viz import (plot_bursting, plot_power, plot_spectrogram,
+                       plot_group_bursting, plot_group_power,
+                       plot_burst_shape)
 
 config = get_config()
 layout = get_layout()
@@ -22,6 +24,8 @@ overwrite = \
 np.random.seed(config['seed'])
 
 # loop across subjects
+infos = list()
+these_events_all = dict()
 for eegf in eegfs:
     behf = get_behf(eegf)
     all_indices, slow_indices, fast_indices = slowfast2epochs_indices(behf)
@@ -31,22 +35,40 @@ for eegf in eegfs:
                           for event in my_events()}
     events = get_events(raw, exclude_events=epo_reject_indices)
     raw = pick_data(raw)
+    infos.append(raw.info)
+    these_events_all[eegf.path] = dict()
     for event in events:
         indices = np.random.choice(range(events[event].shape[0]), 10)
         these_events = select_events(events[event], indices,
                                      epo_reject_indices[event])
-        plot_spectrogram(eegf, raw, event, these_events)
+        plot_spectrogram(eegf, raw, event, these_events, overwrite=overwrite)
     for name, indices in {'All': all_indices, 'Slow': slow_indices,
                           'Fast': fast_indices}.items():
+        these_events_all[eegf.path][name] = dict()
         for event in events:
             these_events = select_events(events[event], indices,
                                          epo_reject_indices[event])
-            plot_beta_bursting(eegf, name, event, raw.info,
-                               raw.ch_names, these_events,
-                               overwrite=overwrite)
-            plot_beta_bursting(eegf, name, event, raw.info,
-                               raw.ch_names, these_events,
-                               picks=['C3', 'C4'], overwrite=overwrite)
+            these_events_all[eegf.path][name][event] = these_events
+            plot_bursting(eegf, name, event, raw.info,
+                          these_events, overwrite=overwrite)
+            plot_bursting(eegf, name, event, raw.info, these_events,
+                          picks=['C3', 'C4'], overwrite=overwrite)
             plot_power(eegf, name, event, raw.info,
-                       raw.ch_names, these_events,
-                       overwrite=overwrite)
+                       these_events, overwrite=overwrite)
+            plot_power(eegf, name, event, raw.info,
+                       these_events, picks=['C3', 'C4'], overwrite=overwrite)
+            plot_burst_shape(eegf, name, event, raw.info,
+                             these_events, overwrite=overwrite)
+
+for name, indices in {'All': all_indices, 'Slow': slow_indices,
+                      'Fast': fast_indices}.items():
+    for event in events:
+        plot_group_bursting(eegfs, name, event, infos,
+                            these_events_all, overwrite=overwrite)
+        plot_group_bursting(eegf, name, event, infos, these_events_all,
+                            picks=['C3', 'C4'], overwrite=overwrite)
+        plot_group_power(eegfs, name, event, infos,
+                         these_events_all, overwrite=overwrite)
+        plot_group_power(eegfs, name, event, infos,
+                         these_events_all, picks=['C3', 'C4'],
+                         overwrite=overwrite)
